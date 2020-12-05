4c730da569613c9774cbe17170526e09623f50cc
Carlos Cabas <ing.carloscabas@gmail.com>
2020-12-05 08:15:56 -0500

    commit inicial

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,13 @@
+def convert(number):
+    output = ''
+    if number % 3 == 0:
+        output += 'Pling'
+    if number % 5 == 0:
+        output += 'Plang' 
+    if number % 7 == 0:
+        output += 'Plong'
+
+    if len(output) == 0:
+        return(str(number))
+    else:
+        return(output)
